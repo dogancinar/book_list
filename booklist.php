9b6584fcbefa3ddcfcdbe7b9c09f46cd9f5c9213
Doğan Çınar <62171120+dogancinar@users.noreply.github.com>
2020-05-06 01:30:52 +0300

    Update booklist.php

--- booklist.php
+++ booklist.php
@@ -4,11 +4,6 @@
   <?php  if (@$_GET['durum']=='ok') { ?><script>Swal.fire('Başarılı','','success')</script></b>
                                 <?php   }else if(@$_GET['durum']=='no') { ?>  <script>Swal.fire(' Başarısız','','error')</script>
                                 <?php  } ?>
-
-
-
-
-
 <br>
 <div class="container">
 
@@ -45,36 +40,17 @@
  
           <?php
           }
-          
-
-
 
       }
-
-      
-      
-      
-      ?>
+ ?>
 
     </tr>
-          
-   
-      
-  
+
   </tbody>
 
 </table>
 </div>
 
-
-
-
-
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    
 
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
