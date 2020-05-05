c80d660e2b02d072e1a3b57ad76ea5c18a84f7f6
Doğan Çınar <62171120+dogancinar@users.noreply.github.com>
2020-05-06 01:29:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
-<?php 	include 'include/header.php'
-?>
-
-
-
-
-  <body>
-
-
-
-
-
+<?php 	include 'include/header.php'?>
+<body>
 <div class="container">
   
 <div class="card" style="width: 50rem; margin-top:30px;margin-left:100px;"  >
@@ -63,27 +53,10 @@
   </ul><br>
   </form>
 
-
 </div>
 
 </div>
-
-
-
-
 <br>
-
-
-
-
-
-
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    
-
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
